72d584be770edeca9c28dfe732bd0bb3ed2688f3
Kevin Finnegan <kefinnegan@deloitte.ie>
2023-10-09 17:47:55 +0100

    Refactored TestInvoker to use RunImpersonatedAsync

--- src/xUnit.UserContext/XunitExtensions/UserContextTestInvoker.cs
+++ src/xUnit.UserContext/XunitExtensions/UserContextTestInvoker.cs
@@ -1,8 +1,13 @@
-﻿using SimpleImpersonation;
-using System;
+﻿using System;
 using System.Collections.Generic;
+using System.Net;
 using System.Reflection;
+using System.Runtime.Versioning;
+using System.Security.Principal;
 using System.Threading;
+using System.Threading.Tasks;
+using Microsoft.Win32.SafeHandles;
+using SimpleImpersonation;
 using Xunit.Abstractions;
 using Xunit.Sdk;
 using Xunit.UserContext.Configuration;
@@ -29,22 +34,35 @@ namespace Xunit.UserContext.XunitExtensions
         /// <param name="aggregator">The exception aggregator used to run code and collect exceptions.</param>
         /// <param name="cancellationTokenSource">The task cancellation token source, used to cancel the test run.</param>
         /// <param name="userContext">The user context settings for impersonation</param>
-        public UserContextTestInvoker(ITest test, IMessageBus messageBus, Type testClass, object[] constructorArguments, MethodInfo testMethod, object[] testMethodArguments, IReadOnlyList<BeforeAfterTestAttribute> beforeAfterAttributes, ExceptionAggregator aggregator, CancellationTokenSource cancellationTokenSource, UserContextSettings userContext)
-            : base(test, messageBus, testClass, constructorArguments, testMethod, testMethodArguments, beforeAfterAttributes, aggregator, cancellationTokenSource)
+        public UserContextTestInvoker(ITest test, IMessageBus messageBus, Type testClass, object[] constructorArguments,
+            MethodInfo testMethod, object[] testMethodArguments, IReadOnlyList<BeforeAfterTestAttribute> beforeAfterAttributes,
+            ExceptionAggregator aggregator, CancellationTokenSource cancellationTokenSource, UserContextSettings userContext)
+
+            : base(test, messageBus, testClass, constructorArguments, testMethod,
+                  testMethodArguments, beforeAfterAttributes, aggregator, cancellationTokenSource)
         {
             _userContext = userContext;
         }
 
         /// <summary>
-        /// Run test under the context of the provided user
-        /// by wrapping the base class invocation
+        /// Invokes the test method under the context of the provided user on the given test class instance. This method sets up support
+        /// for "async void" test methods, ensures that the test method has the correct number of arguments, then calls <see cref="CallTestMethod"/>
+        /// to do the actual method invocation. It ensure that any async test method is fully completed before returning, and
+        /// returns the measured clock time that the invocation took.
         /// </summary>
-        /// <param name="testClassInstance">The instance of the test class</param>
-        /// <returns>The return value from the test method invocation</returns>
-        protected override object CallTestMethod(object testClassInstance)
+        /// <param name="testClassInstance">The test class instance</param>
+        /// <returns>Returns the time taken to invoke the test method</returns>
+        [SupportedOSPlatform("windows")]
+        protected override async Task<decimal> InvokeTestMethodAsync(object testClassInstance)
         {
-            return Impersonation.RunAsUser(_userContext.GetCredentials(), _userContext.LogonType,
-                () => base.CallTestMethod(testClassInstance));
+            decimal timerTotal = default;
+
+            using (SafeAccessTokenHandle userHandle = _userContext.GetCredentials().LogonUser(_userContext.LogonType))
+            {
+                timerTotal = await WindowsIdentity.RunImpersonatedAsync(userHandle, async () => await base.InvokeTestMethodAsync(testClassInstance));
+            }
+
+            return timerTotal;
         }
     }
 }
